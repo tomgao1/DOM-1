5eae445c8b52566b5dba07d517e8a7fb78787a3b
tomgao <406038564@qq.com>
2020-04-30 21:45:24 +0800

    第一次封装

---
+++ src/zuoye.js
@@ -0,0 +1,27 @@
+window.dom = { 
+    style(node,name,value){
+        if(arguments.length === 3){
+             //dom.style(div,'color','red')
+             node.style[name] = value
+        }else if(arguments.length === 2){
+            if(typeof name === 'string'){
+                //dom.style(div,'color')
+                return node.style[name]
+            }else if(name instanceof Object){
+                //dom.style(div,{color: 'red'})
+                const object = name 
+                for (let key in object){
+                    node.style[key] =object[key]
+                } 
+            }
+        }
+    },//作业2
+            find(selector,scope){
+                return (scope|| document).querySelectorAll(selector)
+            }, //作业1
+            each(nodeList,fn){
+                for(let i=0;i<nodeList.length;i++){
+                    fn.call(null,nodeList[i]) //作业3
+                }
+            },
+        }
